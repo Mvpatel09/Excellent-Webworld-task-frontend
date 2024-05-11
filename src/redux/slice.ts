d7cd58c215fc8be437c918afd831458c1e0948d0
Mvptel09 <maulikvarasani@gmail.com>
2024-05-11 17:36:21 +0530

    add product component

--- src/redux/slice.ts
+++ src/redux/slice.ts
@@ -14,10 +14,28 @@ const UserSliceData = createSlice({
             state.token = payload.token;
             localStorage.setItem('token', payload.token);
         },
+        addQty: (state, { payload }) => {
+            const getIndex = state.cart.findIndex((element) => element.id === payload.id);
+            if (getIndex === -1) {
+                state.cart = [...state?.cart, { ...payload, qty: 1 }];
+            } else {
+                state.cart[getIndex].qty += 1;
+            }
+        },
+        minusQty: (state, { payload }) => {
+            const getIndex = state.cart.findIndex((element) => element.id === payload.id);
+            if (state.cart[getIndex]?.qty === 1) {
+                state.cart = state.cart.filter((element) => element.id !== payload.id)
+            } else {
+                state.cart[getIndex].qty -= 1;
+            }
+        },
+        removeQty: (state, { payload }) => {
 
+        }
     },
 });
 
-export const { logout, login } = UserSliceData.actions;
+export const { logout, login, addQty, minusQty } = UserSliceData.actions;
 
 export default UserSliceData.reducer;
